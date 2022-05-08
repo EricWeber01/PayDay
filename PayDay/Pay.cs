1c2cc5bc33230c5b8661731f8fda13e3634a5281
babya <babya@ERIC_WEBER>
2022-05-08 20:03:16 +0300

    Add project files.

---
+++ PayDay/Pay.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace payday
+{
+    [Serializable]
+    public class Pay
+    {
+        public int Money { get; set; }
+        public int Days { get; set; }
+        public int Fine { get; set; }
+        public int FineDays { get; set; }
+
+        public int PayWithoutFine { get; set; }
+        public int GeneralFine { get; set; }
+        public int GeneralPay { get; set; }
+        public Pay() { }
+
+        public Pay(int m, int d, int f, int fd)
+        {
+            Money = m;
+            Days = d;
+            Fine = f;
+            FineDays = fd;
+
+            PayWithoutFine = Money * Days;
+            GeneralFine = Fine * FineDays;
+            GeneralPay = PayWithoutFine + GeneralPay;
+
+
+        }
+        public override string ToString()
+        {
+            return $"Плата за день {Money}\n" +
+                $"Кол-во дней {Days}\n" +
+                $"Штраф {Fine}\n" +
+                $"Кол-во дней {FineDays}\n" +
+                $"Плата без штрафов {PayWithoutFine} \n" +
+                $"Общая сумма штрафов {GeneralFine} \n" +
+                $"Общая плата {GeneralPay} \n";
+        }
+    }
+}
---
+++ PayDay/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+namespace payday
+{
+    class Program
+    {
+        public static void Serialize(Pay p)
+        {
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true
+            };
+            var jsonString = JsonSerializer.Serialize<Pay>(p, options);
+            File.WriteAllText("Payment.json", jsonString);
+            XmlSerializer xml = new XmlSerializer(typeof(Pay));
+            using (FileStream fs = new FileStream("Pay.xml", FileMode.OpenOrCreate))
+            {
+                xml.Serialize(fs, p);
+            }
+
+        }
+        public static Pay JsonDeSerialize()
+        {
+            Pay tmp = new Pay();
+            var jsonString = File.ReadAllText("Payment.json");
+
+
+            tmp = JsonSerializer.Deserialize<Pay>(jsonString);
+
+
+            return tmp;
+        }
+        public static Pay XMLDeSerialize()
+        {
+            Pay tmp = new Pay();
+            XmlSerializer xml = new XmlSerializer(typeof(Pay));
+            using (FileStream fs = new FileStream("Pay.xml", FileMode.OpenOrCreate))
+            {
+
+                tmp = xml.Deserialize(fs) as Pay;
+            }
+
+            return tmp;
+        }
+        static void Main(string[] args)
+        {
+            Pay p = new Pay(50, 10, 15, 3);
+
+            Serialize(p);
+            Pay p1 = JsonDeSerialize();
+            Pay p2 = XMLDeSerialize();
+            Console.WriteLine(p1);
+            Console.WriteLine("\n---------------------\n");
+            Console.WriteLine(p2);
+        }
+    }
+}
